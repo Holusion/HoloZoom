e13c520c19ad9b20c0cd6e8c956550b3a4904ed5
Holusion <holusion@Mustafar.local>
2019-02-14 15:56:02 +0100

    BUGFIX: touching UI didn't work with Ipad

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,7 +25,15 @@ public class Player : NetworkBehaviour {
     }
 
     public bool IsUI() {
-        return EventSystem.current != null && EventSystem.current.IsPointerOverGameObject();
+        return EventSystem.current != null && this.IsPointerOverUIObject();
+    }
+
+    private bool IsPointerOverUIObject() {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 
     [Command]
