a96d1db2c8cb68fe7408e50ffa90d984ee8d9ff2
YannDub <yann59.dubois@gmail.com>
2019-02-21 11:40:09 +0100

    Add documentation for ITargetAnswer

--- Assets/Scripts/Target/ITargetAnswer.cs
+++ Assets/Scripts/Target/ITargetAnswer.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public interface ITargetAnswer
 {
+    // Is called when a selection on the current game object is detected (CmdTarget(SELECT, gameObject))
     void OnSelected(GameObject previousTarget);
+    // Is called when a reset on the current game object is detected (CmdTarget(UNSELECT, gameObject))
     void OnUnselected(GameObject previouslastTarget);
+    // Is called when "enable" change (CmdEnable(gameObject, enable))
     void OnActive(bool enable);
+    // Method to use to inform Unity that this.gameObject should be disable (for exemple, event animation) 
     void OnDesactive();
 }
