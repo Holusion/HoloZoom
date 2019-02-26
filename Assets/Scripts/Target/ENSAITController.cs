363a29da74f10e3ab5d85d02cc2c036e5b1a9605
YannDub <yann59.dubois@gmail.com>
2019-02-26 16:57:31 +0100

    Add button for reset animation on ENSAIT

--- Assets/Scripts/Target/ENSAITController.cs
+++ Assets/Scripts/Target/ENSAITController.cs
@@ -6,16 +6,29 @@ using UnityEngine.UI;
 public class ENSAITController : TargetAnimationController, ITargetAnswer
 {
     public Button elecButton;
+    public Button reset;
 
     public new void OnSelected(GameObject previousTarget) {
         base.OnSelected(previousTarget);
 
         elecButton.gameObject.SetActive(true);
+        elecButton.onClick.AddListener(() => {
+            reset.gameObject.SetActive(true);
+            elecButton.gameObject.SetActive(false);
+        });
+
+        reset.onClick.AddListener(() => {
+            elecButton.gameObject.SetActive(true);
+            reset.gameObject.SetActive(false);
+        });
     }
 
     public new void OnUnselected(GameObject previousTarget) {
         base.OnUnselected(previousTarget);
 
         elecButton.gameObject.SetActive(false);
+        Animator anim = this.gameObject.GetComponent<Animator>();
+        int trigger = Animator.StringToHash("Open");
+        anim.SetTrigger(trigger);
     }
 }
