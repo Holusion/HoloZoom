96caca6d225367c0f88b8b8e3bf63f5a0b26ae7a
YannDub <yann59.dubois@gmail.com>
2019-02-26 16:31:36 +0100

    Add electricity animation

---
+++ Assets/Scripts/Target/ENSAITController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ENSAITController : TargetAnimationController, ITargetAnswer
+{
+    public Button elecButton;
+
+    public new void OnSelected(GameObject previousTarget) {
+        base.OnSelected(previousTarget);
+
+        elecButton.gameObject.SetActive(true);
+    }
+
+    public new void OnUnselected(GameObject previousTarget) {
+        base.OnUnselected(previousTarget);
+
+        elecButton.gameObject.SetActive(false);
+    }
+}
