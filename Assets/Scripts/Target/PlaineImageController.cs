4317a5a9993d1540ad3cccd585cae2bc52393898
YannDub <yann59.dubois@gmail.com>
2019-02-26 10:21:25 +0100

    Add button that can lauch animation

---
+++ Assets/Scripts/Target/PlaineImageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaineImageController : TargetAnimationController, ITargetAnswer
+{
+    public Button _2020;
+    public Button _2021;
+
+    public new void OnSelected(GameObject previousTarget) {
+        base.OnSelected(previousTarget);
+
+        _2020.gameObject.SetActive(true);
+        _2021.gameObject.SetActive(true);
+    }
+
+    public new void OnUnselected(GameObject previousTarget) {
+        base.OnUnselected(previousTarget);
+
+        _2020.gameObject.SetActive(false);
+        _2021.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/ButtonLaunchAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLaunchAnimation : MonoBehaviour, ButtonAction
+{
+    public GameObject objectToAnimate;
+    public string trigger;
+
+    public void MakeAction(Player player)
+    {
+        int iTrigger = Animator.StringToHash(trigger);
+        Animator anim = objectToAnimate.GetComponent<Animator>();
+        anim.SetTrigger(iTrigger);
+    }
+}
