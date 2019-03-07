eb336245b62c2f6cbf4c171c7d0f1201adef93e6
YannDub <yann59.dubois@gmail.com>
2019-03-07 17:32:36 +0100

    Add TreeController

---
+++ Assets/Scripts/Target/TreeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeController : MonoBehaviour, ITargetAnswer
+{
+    public Text treeUI;
+
+    public void OnActive(bool enable)
+    {
+
+    }
+
+    public void OnDesactive()
+    {
+
+    }
+
+    public void OnRotate()
+    {
+
+    }
+
+    public void OnSelected(GameObject previousTarget)
+    {
+        treeUI.text += this.name + "<b>/</b>";
+    }
+
+    public void OnUnselected(GameObject previouslastTarget)
+    {
+        treeUI.text = treeUI.text.Replace(this.name + "<b>/</b>", "");
+    }
+}
