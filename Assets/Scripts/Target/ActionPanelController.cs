ae2bff7c0df6ab113d476910c49c00dac50c3666
YannDub <yann59.dubois@gmail.com>
2019-03-08 14:17:48 +0100

    enable/disable init panel

--- Assets/Scripts/Target/ActionPanelController.cs
+++ Assets/Scripts/Target/ActionPanelController.cs
@@ -6,16 +6,24 @@ using UnityEngine.UI;
 public class ActionPanelController : MonoBehaviour, ITargetAnswer
 {
     public GameObject actionPanel;
+    GameObject initPanel;
 
     void Start() {
         Transform background = actionPanel.transform.Find("Panel").Find("Background");
+        initPanel = GameObject.Find("ActionPanelInitPos");
     }
 
     public void OnSelected(GameObject previousTarget) {
+        if(previousTarget == GameObject.Find("InitPos")) {
+            initPanel.SetActive(false);
+        }
         actionPanel.SetActive(true);
     }
 
     public void OnUnselected(GameObject previousTarget) {
+        if(previousTarget == GameObject.Find("InitPos")) {
+            initPanel.SetActive(true);
+        }
         actionPanel.SetActive(false);
     }
 
