4953635545fd7693bb8e71dcaf612c140f102c2a
YannDub <yann59.dubois@gmail.com>
2019-03-14 10:24:29 +0100

    Add GameManager to automatize activation of game object in function isClient

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GameManager : MonoBehaviour
+{
+    public bool isClient;
+
+    public GameObject client;
+    public GameObject[] interactiveObjectWithBubbleText;
+    public NetworkHolusion networkHolusion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.UpdateView();
+    
+    }
+
+    void Update() {
+        this.UpdateView();
+    }
+
+    void UpdateView() {
+        this.client.SetActive(isClient);
+        this.networkHolusion.isClient = !this.isClient;
+
+        foreach(GameObject go in interactiveObjectWithBubbleText) {
+            go.transform.Find("BubbleText").gameObject.SetActive(!this.isClient);
+        }
+
+        Camera.main.clearFlags = isClient ? CameraClearFlags.Skybox : CameraClearFlags.SolidColor;
+    }
+}
