e520b8d72e9e348d77c9c2e326006b4e124ed2e0
YannDub <yann59.dubois@gmail.com>
2019-03-25 10:37:08 +0100

    Update GameManage

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
+[ExecuteInEditMode]
 public class GameManager : MonoBehaviour
 {
     public bool isClient;
-    public GameObject client;
+    public GameObject canvas;
     public GameObject[] interactiveObjectWithBubbleText;
     public NetworkHolusion networkHolusion;
 
@@ -15,22 +16,23 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {    
-        this.UpdateView();
+        // this.UpdateView();
     }
 
     void Update() {
-        if(Camera.main != null && !this.skyChange) {
-            this.skyChange = true;
-        }
+        // if(Camera.main != null && !this.skyChange) {
+        //     this.skyChange = true;
+        // }
+        this.UpdateView();
     }
 
     void UpdateView() {
-        // this.client.SetActive(isClient);
-        // this.networkHolusion.isClient = !this.isClient;
+        this.canvas.SetActive(isClient);
+        this.networkHolusion.isClient = !this.isClient;
 
         foreach(GameObject go in interactiveObjectWithBubbleText) {
             go.transform.Find("BubbleText").gameObject.SetActive(!this.isClient);
-            Camera.main.clearFlags = isClient ? CameraClearFlags.Skybox : CameraClearFlags.SolidColor;
         }
+        Camera.main.clearFlags = isClient ? CameraClearFlags.Skybox : CameraClearFlags.SolidColor;
     }
 }
